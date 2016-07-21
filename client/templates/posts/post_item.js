87b50759cc84637f36925a1bcf93bae0bf544ad4
billmcauliff <bill@adamsapple.com>
2016-07-21 08:57:04 -0400

    Voting - pg 253

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -8,18 +8,37 @@ Template.postItem.helpers({
     a.href = this.url;
     return a.hostname;
   },
+
+
   // commentsCount: function() {
   //   return Comments.find({postId: this._id}).count();
   // },
-  commentsSingularOrPlural: function(commentsCount){
-      var comment = {};
-          if (Comments.find({postId: this._id}).count() === 1){
-            comment = "comment";
-          } else {
-            comment = "comments";
-          }
+
+upvotedClass: function() {
+    var userId = Meteor.userId();
+    if (userId && !_.include(this.upvoters, userId)) {
+      return 'btn-primary upvotable';
+    } else {
+      return 'disabled';
+    }
+  }, 
+// Removed this because it is addressed in the book - pluralize - registerHelper - Chap 13
+  // commentsSingularOrPlural: function(commentsCount){
+  //     var comment = {};
+  //         if (Comments.find({postId: this._id}).count() === 1){
+  //           comment = "comment";
+  //         } else {
+  //           comment = "comments";
+  //         }
           
-        return comment;
-      }
+  //       return comment;
+  //     }
     }
 );
+
+Template.postItem.events({
+  'click .upvotable': function(e) {
+    e.preventDefault();
+    Meteor.call('upvote', this._id);
+  },
+});
